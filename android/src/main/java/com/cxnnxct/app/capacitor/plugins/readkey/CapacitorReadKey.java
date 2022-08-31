0c463ef8eb3f2700c59d2b5724119ad6c7d598e7
Varsity Software <jweaver@iemic.com>
2022-08-30 20:21:13 -0400

    Create CapacitorReadKey.java

---
+++ android/src/main/java/com/cxnnxct/app/capacitor/plugins/readkey/CapacitorReadKey.java
@@ -0,0 +1,11 @@
+package com.cxnnxct.app.capacitor.plugins.readkey;
+
+import android.util.Log;
+
+public class CapacitorReadKey {
+
+    public String read(String value) {
+        Log.i("Value", value);
+        return value;
+    }
+}
