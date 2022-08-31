a069d6ae8eb5677e7f515fd1909f5c017281c880
Varsity Software <jweaver@iemic.com>
2022-08-30 20:21:29 -0400

    Create CapacitorReadKeyPlugin.java

---
+++ android/src/main/java/com/cxnnxct/app/capacitor/plugins/readkey/CapacitorReadKeyPlugin.java
@@ -0,0 +1,40 @@
+package com.cxnnxct.app.capacitor.plugins.readkey;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+import android.app.Activity;
+
+@CapacitorPlugin(name = "CapacitorReadKey")
+public class CapacitorReadKeyPlugin extends Plugin {
+
+    private CapacitorReadKey implementation = new CapacitorReadKey();
+
+    @PluginMethod
+    public void read(PluginCall call) {
+        //String value = call.getString("value");
+
+        //JSObject ret = new JSObject();
+        //ret.put("value", implementation.echo(value));
+        //call.resolve(ret);
+
+        String key = call.getString("key");
+
+        final Activity activity = getActivity();
+
+        String packageName = activity.getPackageName();
+
+        int resId = activity.getResources().getIdentifier(key, "string", packageName);
+        String value = activity.getString(resId);
+
+        JSObject ret = new JSObject();
+        //ret.put("value", value);
+        //call.success(ret);
+
+        ret.put("value", implementation.echo(value));        
+        call.resolve(ret);
+    }
+}
